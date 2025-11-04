fe1fcb1eb9d4d5faa77015ac8d87c84859c2660b
satishthapak <satishthapak@outlook.com>
2025-11-04 15:31:52 +0530

    created nat_gatway

--- root/main.tf
+++ root/main.tf
@@ -10,3 +10,16 @@ module "aws_vpc" {
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
+
+module "nat_gateway" {
+  source = "../modules/nat_gateway"
+  project_name                = var.project_name
+  vpc_id                      = module.aws_vpc.vpc_id
+  internet_gateway_id         = module.aws_vpc.internet_gateway_id
+  public_subnet_az1_id        = module.aws_vpc.public_subnets[0]
+  public_subnet_az2_id        = module.aws_vpc.public_subnets[1]
+  private_app_subnet_az1_id   = module.aws_vpc.private_app_subnets[0]
+  private_app_subnet_az2_id   = module.aws_vpc.private_app_subnets[1]
+  private_data_subnet_az1_id  = module.aws_vpc.private_db_subnets[0]
+  private_data_subnet_az2_id  = module.aws_vpc.private_db_subnets[1]
+}
--- root/variable.tf
+++ root/variable.tf
@@ -37,3 +37,7 @@ variable "private_data_subnet_az2_cidr" {
   type        = string
   description = "CIDR block for private DB subnet in AZ2"
 }
+
+
+
+
