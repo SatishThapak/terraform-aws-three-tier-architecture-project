67382b8ba89c582535e84b7e5d761e25983dcece
satishthapak <satishthapak@outlook.com>
2025-11-05 12:19:12 +0530

    created security groups

--- root/main.tf
+++ root/main.tf
@@ -27,5 +27,5 @@ module "nat_gateway" {
 module "security_groups" {
   source       = "../modules/security_groups"
   project_name = var.project_name
-  vpc_id       = module.vpc.vpc_id
+  vpc_id       = module.aws_vpc.vpc_id
 }
